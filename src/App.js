8c96111809411fa17fcc8e312001d5ed1e3de8fb
Javier Palacios <javierpalaciosbotejara@gmail.com>
2022-02-26 08:53:37 +0100

    Add login style

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from "react";
 import "./styles.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./views/main/Main";
-import Nav from "./components/nav/Nav";
+import Nav from "./components/nav/nav";
+import Login from "./views/login/login";
 
 const App = () => {
   return (
     <div className="container">
-       <BrowserRouter>
-      <Routes>
+      <BrowserRouter>
+        <Routes>
           <Route path="/" element={<Main />} />
-      </Routes>
-      <Nav></Nav>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        {/* <Nav></Nav> */}
       </BrowserRouter>
     </div>
   );
--- src/views/login/login.jsx
+++ src/views/login/login.jsx
@@ -1,1 +1,47 @@
-//  login
+import React from "react";
+
+import { TextField, Button } from "@mui/material";
+import logo from "../../assets/img/thebridgelogo.png";
+
+const Login = () => {
+  const userLogin = () => {
+    //GET INPUT INFO
+    //VALIDATE
+    //ENCRYPT PASSWORD
+    //SEND TO BACKEND
+    //AWAIT RESPONSE
+    //SUCCESS/ERROR MESSAGE
+    //REPIT/REDIRECT
+  };
+
+  return (
+    <>
+      <div className="nav login">
+        <div className="nav-logo-container">
+          <img src={logo} alt="logo" />
+          <h2>ASSISTANCE</h2>
+        </div>
+      </div>
+      <div className="login-field">
+        <img src={logo} alt="logo" />
+        <h2>INICIA SESION</h2>
+        <TextField variant="standard" label="Usuario" />
+        <TextField variant="standard" label="Contraseña" />
+        <Button
+          onClick={() => userLogin()}
+          variant="contained"
+          size="large"
+          sx={{
+            paddingX: 10,
+          }}
+        >
+          Login
+        </Button>
+      </div>
+    </>
+  );
+};
+export default Login;
+//! BORDER RADIUS HARDCORED
+//! WRONG LOGO
+//* VALIDATE -> AXIOS -> BACk -> DB -> RESP
