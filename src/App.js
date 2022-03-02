275c6e800f6f3c58d1fadb73175f3a0f49653198
Javier Palacios <javierpalaciosbotejara@gmail.com>
2022-03-02 14:31:51 +0100

    Add componnet courses

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
-import "./styles.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "./views/main/Main";
-import Nav from "./components/nav/nav";
+
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./assets/themeCofi";
+import "./styles.scss";
+
+import Main from "./views/main/Main";
+import { Courses } from "views/courses";
+import Nav from "./components/nav/nav";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      {/* <BrowserRouter> */}
-      {/* <Routes>
+      <BrowserRouter>
+        <Routes>
           <Route path="/" element={<Main />} />
-      </Routes>
-      <Nav></Nav>
-      {/* </BrowserRouter> */}
+          <Route path="/courses/:user" element={<Courses />} />
+        </Routes>
+        <Nav></Nav>
+      </BrowserRouter>
     </ThemeProvider>
   );
 };
