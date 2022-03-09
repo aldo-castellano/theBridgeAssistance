97c3f1a93102895e46b393f3fedb85b80c04d434
Sebastian M <ra3gan1991@gmail.com>
2022-03-09 13:05:52 +0100

    Acabo from de crear curso

--- src/components/forms/CourseForm.jsx
+++ src/components/forms/CourseForm.jsx
@@ -4,119 +4,143 @@ import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import { format } from "date-fns";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../../assets/formConfi";
-import { Controller , useForm} from "react-hook-form";
-import { Button,TextField } from "@mui/material";
+import { Controller, useForm } from "react-hook-form";
+import { Button, TextField, Select, MenuItem ,InputLabel,FormControl} from "@mui/material";
 import * as yup from "yup";
-import { yupResolver} from "@hookform/resolvers/yup";
-
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const schemaCourse = yup.object().shape({
   title: yup
     .string()
     .min(3, "El titulo debe tener un minimo de 3 caracteres")
     .required("Este campo es obligario"),
-    dateStart: yup.date().required("Es necesario este campo"),
-    dateEnd:yup.date().min(yup.ref("dateStart"),"La fecha de fin debe de ser mayor a la de inicio")
+  dateStart: yup.date().required("Es necesario este campo"),
+  dateEnd: yup
+    .date()
+    .min(
+      yup.ref("dateStart"),
+      "La fecha de fin debe de ser mayor a la de inicio"
+    ),
 });
 
-export default function CourseForm({setForm,title}) {
+export default function CourseForm({ setForm, title }) {
   const {
-    control:controlCourse,
+    control: controlCourse,
     handleSubmit,
-    formState: { errors }
+    formState: { errors },
   } = useForm({
-    resolver:yupResolver(schemaCourse)
+    resolver: yupResolver(schemaCourse),
   });
 
-  const onSubmit = data => setForm(data)
+  const onSubmit = (data) => setForm(data);
   return (
     <>
-     <div className="add-form-header">
+      <div className="add-form-header">
         <h2>{title}</h2>
       </div>
-      
-      <form onSubmit={handleSubmit(onSubmit)} >
-      <div className="form-contianer">
-      <Controller
-        name="title"
-        control={controlCourse}
-        defaultValue=""
-        render={({ field }) => (
-          <TextField
-            {...field}
-            id="title-input"
-            label="Titulo del curso"
-            variant="standard"
-            error={!!errors.title}
-            helperText={errors.title ? errors.title?.message : ""}
-          />
-        )}
-      />
-      <ThemeProvider theme={theme}>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-contianer">
           <Controller
-            name="dateStart"
+            name="title"
             control={controlCourse}
-            defaultValue={format(Date.now(), "yyyy-MM-dd")}
-            render={({
-              field: { onChange, value },
-              fieldState: { error, invalid },
-            }) => (
-              <DatePicker
-                label="Inicio de curso"
-                id="dateStart"
-                value={value}
-                onChange={(nvalue) => {
-                    console.log(format(nvalue, "yyyy-MM-dd"))
-                    onChange(nvalue)
-                }                  
-                }
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
+            defaultValue=""
+            render={({ field }) => (
+              <TextField
+                {...field}
+                id="title-input"
+                label="Titulo del curso"
+                variant="standard"
+                error={!!errors.title}
+                helperText={errors.title ? errors.title?.message : ""}
+              />
+            )}
+          />
+          <ThemeProvider theme={theme}>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <Controller
+                name="dateStart"
+                control={controlCourse}
+                defaultValue={format(Date.now(), "yyyy-MM-dd")}
+                render={({
+                  field: { onChange, value },
+                  fieldState: { error, invalid },
+                }) => (
+                  <DatePicker
+                    label="Inicio de curso"
                     id="dateStart"
-                    variant="standard"
-                    margin="dense"
-                    error={!!errors.dateStart}
-                    helperText={errors.dateStart ? errors.dateStart?.message : null}
+                    value={value}
+                    onChange={(nvalue) => {
+                      console.log(format(nvalue, "yyyy-MM-dd"));
+                      onChange(nvalue);
+                    }}
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        id="dateStart"
+                        variant="standard"
+                        margin="dense"
+                        error={!!errors.dateStart}
+                        helperText={
+                          errors.dateStart ? errors.dateStart?.message : null
+                        }
+                      />
+                    )}
                   />
                 )}
               />
-            )}
-          />
-          <Controller
-            name="dateEnd"
-            control={controlCourse}
-            defaultValue={format(Date.now(), "yyyy-MM-dd")}
-            render={({
-              field: { onChange, value },
-              fieldState: { error, invalid },
-            }) => (
-              <DatePicker
-                label="Inicio de curso"
-                id="dateEnd"
-                value={value}
-                onChange={(nvalue) => {
-                    console.log(format(nvalue, "yyyy-MM-dd"))
-                    onChange(nvalue)
-                }                  
-                }
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
+              <Controller
+                name="dateEnd"
+                control={controlCourse}
+                defaultValue={format(Date.now(), "yyyy-MM-dd")}
+                render={({
+                  field: { onChange, value },
+                  fieldState: { error, invalid },
+                }) => (
+                  <DatePicker
+                    label="Inicio de curso"
                     id="dateEnd"
-                    variant="standard"
-                    margin="dense"
-                    error={!!errors.dateEnd}
-                    helperText={errors.dateEnd ? errors.dateEnd?.message : null}
+                    value={value}
+                    onChange={(nvalue) => {
+                      console.log(format(nvalue, "yyyy-MM-dd"));
+                      onChange(nvalue);
+                    }}
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        id="dateEnd"
+                        variant="standard"
+                        margin="dense"
+                        error={!!errors.dateEnd}
+                        helperText={
+                          errors.dateEnd ? errors.dateEnd?.message : null
+                        }
+                      />
+                    )}
                   />
                 )}
               />
-            )}
-          />
-        </LocalizationProvider>
-      </ThemeProvider>
-      <Button variant="contained" type="submit" sx={{mt:4}}>Crear</Button></div>
+            </LocalizationProvider>
+            <FormControl >
+          <InputLabel id="demo-simple-select-label">Modalidad</InputLabel>              
+          <Controller
+            name="typeCourse"
+            control={controlCourse}
+            defaultValue={0}
+            
+            render={({ field }) => (
+              <Select {...field} label="Modalidad">
+                <MenuItem value={0}>FullTime</MenuItem>
+                <MenuItem value={1}>PartTime</MenuItem>
+              
+              </Select>
+            )} />
+            </FormControl>
+              </ThemeProvider>
+          <Button variant="contained" type="submit" sx={{ mt: 4 }}>
+            Crear
+          </Button>
+        </div>
       </form>
     </>
   );
