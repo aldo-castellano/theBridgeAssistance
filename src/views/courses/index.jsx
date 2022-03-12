f2cdbf2c73341b4feb0347e42f8725dbdc0c39f6
Javier Palacios <javierpalaciosbotejara@gmail.com>
2022-03-12 01:55:16 +0100

    Link views based on role

--- src/views/courses/index.jsx
+++ src/views/courses/index.jsx
@@ -1,40 +1,56 @@
 import Context from "context/UserContext";
 import { useSession } from "logic/useSession";
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import usercourse from "services/usercourses";
+import userCourses from "services/usercourses";
+import adminCourses from "services/admincourses";
+import axios from "axios";
 
 export const Courses = () => {
   const navigate = useNavigate();
-  const { isLogged, logout } = useSession();
+  const { isLogged } = useSession();
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     if (!isLogged) navigate("/login");
   }, [isLogged, navigate]);
 
-  const { user } = useContext(Context);
-  const userid = user.split(",")[0];
-  const courses = usercourse(userid);
+  const [courses, setCourses] = useState([]);
 
-  //? course creation only for admins
-  //? course asignement to users
+  //Wait for user
+  const { user } = useContext(Context);
+  const admin = typeof user == "string" ? user.split(",")[1] == "admin" : false;
+  const userid = typeof user == "string" ? user.split(",")[0] : "null";
+
+  useEffect(async () => {
+    if (admin) setCourses(await adminCourses());
+    else setCourses(userCourses(userid));
+  }, [admin]);
+
+  const handleClick = (id, title) => {
+    if (admin) navigate("/add-course", { state: { id, title } });
+    else navigate("/class", { state: { id, coursename: title } });
+  };
 
   return (
     <>
       <h2 className="title">MIS CURSOS</h2>
       <div className="courses-container">
-        <section onClick={() => navigate("/")} className="course plus">
-          <p>+</p>
-        </section>
-        {courses.map(({ id, title }, i) => (
-          <section
-            key={`course-${i}`}
-            onClick={() => navigate("/", { state: { id, title } })}
-            className="course"
-          >
-            <p>{title}</p>
+        {admin && (
+          <section onClick={() => navigate("/")} className="course plus">
+            <p>+</p>
           </section>
-        ))}
+        )}
+        {courses[0] &&
+          courses.map(({ id, title }, i) => (
+            <section
+              key={`course-${i}`}
+              onClick={() => handleClick(id, title)}
+              className="course"
+            >
+              <p>{title}</p>
+            </section>
+          ))}
       </div>
     </>
   );
